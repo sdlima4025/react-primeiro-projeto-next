24b77fb4f827ff09cf1141052f7be1e7869a8059
sdlima4025 <sergiolimafg3@gmail.com>
2023-12-25 18:17:12 -0300

    criado logica do post no Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,41 @@
 import { LoggedUserContext } from "@/contexts/LoggedUser";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 export const Header = () => {
-  const loggedUserCtx = useContext(LoggedUserContext);
+  const[titleInput, setTitleInput] = useState('');
+  const[bodyInput, setBodyInput] = useState('');
 
-  const handleLogout = () => {
-    loggedUserCtx?.setName("");
-  };
+  const handleAddButton = () => {
+    if(titleInput && bodyInput) {
+      
+    }
+  }
+
+  // const loggedUserCtx = useContext(LoggedUserContext);
+
+  // const handleLogout = () => {
+  //   loggedUserCtx?.setName("");
+  // };
   return (
     <header>
       <h1 className="text-3xl">Título da Página</h1>
+
+      <div className="max-w-xl flex flex-col gap-3 border border-dotted border-gray-400 p-3 my-4">
+        <input 
+          type="text" 
+          placeholder="Titulo do Post"
+          className="border border-gray-300 p-2 text-black text-xl"
+          value={titleInput}
+          onChange={e => setTitleInput(e.target.value)}
+        />
+        <textarea 
+          placeholder="Corpo do post"
+          className="h-24 border border-gray-300 p-2 text-black text-xl"
+          value={bodyInput}
+          onChange={e => setBodyInput(e.target.value)}
+        ></textarea>
+        <button onClick={handleAddButton} className="bg-blue-500 p-3 text-white rounded-md">Adicionar</button>
+      </div>
       {/* {loggedUserCtx?.name && (
         <>
           <p>Usuário Logado: {loggedUserCtx?.name} </p>
